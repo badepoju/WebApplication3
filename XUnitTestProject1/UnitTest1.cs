807044bf1897c4b30f5f337e93084ae6df14341e
badepoju <54426099+badepoju@users.noreply.github.com>
2019-09-01 17:34:54 -0500

    updated the unit test

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -6,7 +6,13 @@ namespace XUnitTestProject1
 {
     public class UnitTest1
     {
-        ValuesController controller = new ValuesController();        [Fact]        public void GetResultValues()        {            var returnValue = controller.Get(1);            Assert.Equal("300", returnValue.Value);        }
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void GetResultValues()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("120", returnValue.Value);
+        }
 
         [Fact]
         public void Test1()
